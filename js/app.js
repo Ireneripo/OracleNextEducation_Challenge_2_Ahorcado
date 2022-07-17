31a488231677145721e103a9b2859eb413848b14
Irene Riportella <riportellairene@gmail.com>
2022-07-17 16:43:31 +0200

    created JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let word;
+let wrongAttempts = 0;
+let rightAttempts = 0;
 let words = [
   'MAMA',
   'PAPA',
@@ -44,24 +47,85 @@ let words = [
   'MESSI',
 ];
 
-// pickRandomWord();
-// function pickRandomWord() {
-//   let word = words[Math.floor(Math.random() * words.length)];
-//   console.log(word);
+const startBtn = document.getElementById('start');
+const hangmanImage = id('hangman-image');
+const letterBtn = document.querySelectorAll('#letters button');
 
-//   let secretWord = word;
-//   return secretWord;
-// }
+startBtn.addEventListener('click', startGame);
 
-let word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
+function id(str) {
+  return document.getElementById(str);
+}
 
-const wordWithUnderlines = word.replace(/./g, '_ ');
+function startGame(event) {
+  id('result').innerHTML = ``;
+  hangmanImage.src = `images/img0.png`;
+  startBtn.disabled = true;
+  wrongAttempts = 0;
+  rightAttempts = 0;
 
-alert(word + ' - ' + wordWithUnderlines);
+  const paragraph = id('secret-word');
+  paragraph.innerHTML = '';
 
-const start = document.querySelector('#start');
-console.log(start);
-start.addEventListener('click', (e) => {
-  console.log(e.target);
-});
+  word = words[Math.floor(Math.random() * words.length)];
+  console.log(word);
+
+  const amountUnderlines = word.length;
+
+  for (let i = 0; i < letterBtn.length; i++) {
+    letterBtn[i].disabled = false;
+  }
+
+  for (let i = 0; i < amountUnderlines; i++) {
+    const span = document.createElement('span');
+    paragraph.appendChild(span);
+  }
+}
+
+for (let i = 0; i < letterBtn.length; i++) {
+  letterBtn[i].addEventListener('click', clickLetter);
+}
+
+function clickLetter(event) {
+  const spans = document.querySelectorAll('#secret-word span');
+  const button = event.target;
+  button.disabled = true;
+  const letter = button.innerHTML.toUpperCase();
+  const wordUpper = word.toUpperCase();
+
+  let correctLetter = false;
+  for (let i = 0; i < wordUpper.length; i++) {
+    if (letter == wordUpper[i]) {
+      spans[i].innerHTML = letter;
+      rightAttempts++;
+      correctLetter = true;
+    }
+  }
+
+  if (correctLetter == false) {
+    wrongAttempts++;
+    const source = `images/img${wrongAttempts}.png`;
+
+    hangmanImage.src = source;
+  }
+
+  if (wrongAttempts == 7) {
+    id('result').innerHTML = `Perdiste, la palabra era ${wordUpper}.`;
+    gameOver();
+  } else if (rightAttempts == wordUpper.length) {
+    id('result').innerHTML = 'GANASTE!!';
+    gameOver();
+  }
+
+  console.log('La letra ' + letter + ' en la palabra ' + wordUpper + ', ¿existe?: ' + correctLetter);
+}
+
+function gameOver() {
+  for (let i = 0; i < letterBtn.length; i++) {
+    letterBtn[i].disabled = true;
+  }
+
+  startBtn.disabled = false;
+}
+
+gameOver();
